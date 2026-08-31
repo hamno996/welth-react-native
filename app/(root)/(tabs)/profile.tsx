dd2c0d7b972d2f241f5ab436644e6ada15104055
hamno996 <hamnomhsen@gmail.com>
2026-09-01 00:12:22 +0300

    signup and signin design and functionality done

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,10 +1,32 @@
+import { useAuth, useUser } from "@clerk/expo";
+import { useRouter } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Alert, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Profile() {
+  const { user } = useUser();
+  const { signOut } = useAuth();
+  const router = useRouter();
+
+  const handleSignOut = () => {
+    Alert.alert("Sign out", "Are you sure you want to sign out?", [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Sign out",
+        style: "destructive",
+        onPress: async () => {
+          await signOut();
+          router.replace("/sign-in");
+        },
+      },
+    ]);
+  };
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-brand-body" edges={["top"]}>
+      <TouchableOpacity>
+        <Text onPress={handleSignOut}>Log out</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
--- lib/schemas/auth.ts
+++ lib/schemas/auth.ts
@@ -16,14 +16,14 @@ export const signUpSchema = z.object({
     ),
 });
 
-export type SignUpFormSchema = z.infer<typeof signUpSchema>;
+export type SignUpFormValues = z.infer<typeof signUpSchema>;
 
 export const signInSchema = z.object({
   email: z.email("Enter a vlaid email.").trim().min(1, "Email is required."),
   password: z.string().min(1, "Password is required"),
 });
 
-export type SignInFormSchema = z.infer<typeof signInSchema>;
+export type SignInFormValues = z.infer<typeof signInSchema>;
 
 export const codeSchema = z.object({
   code: z.string().min(1, "Enter the verification code"),
